9a5d4dc8a26bc1463b61c0aad11604c49555bf3c
Breno Soares <brenosoares4303@gmail.com>
2024-09-10 16:03:10 -0300

    frontend added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+//routes
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from './pages/LoginPage/LoginPage';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
+import Home from './pages/Home/Home';
+
+
+
+const router = createBrowserRouter([
+  {path:'/',
+    element:<Home />,
+    errorElement: <ErrorPage/>
+  },
+
+  {
+    path:'/login',
+    element: <LoginPage/>,
+  }
+])
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
 root.render(
   <React.StrictMode>
-    {/* First component here */}
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
---
+++ frontend/src/pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+function ErrorPage(){
+    return(
+        <div>
+            <p>Error 404!</p>
+        </div>
+    )
+
+}
+export default ErrorPage;
