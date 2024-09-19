f8114b4388fd0f9292a84cadd49c3e8c83dfa406
ThiagoChafado <thiagochafado123@gmail.com>
2024-09-19 17:42:37 -0300

    create loginPage

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,8 +1,22 @@
+import "./Login.css"
+import logo from "../../assets/logo.png" 
 function Login() {
   return (
-    <div>
-      <h1>LOGIN</h1>
+    <div class="mainClass" >
+      <div class="loginDiv">
+        <img id="logo" src={logo} alt="Logo arena"></img>
+      </div>
+      
+      <div class="loginDiv">
+        <input class='inputs'type='text' placeholder='Email'></input>
+        <input class='inputs'type='password' placeholder='Senha'></input>
+        <h1 >Esqueci minha senha</h1>
+        <button id='button'>ENTRAR</button>
+        <button id='forgot'>Esqueci minha senha</button>
+      </div>
     </div>
+    
+    
   );
 }
 export default Login;
