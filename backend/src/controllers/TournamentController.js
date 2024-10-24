5bd5516e78f5c2f61e961f41eee2b2034f243330
ThiagoChafado <thiagochafado123@gmail.com>
2024-10-24 16:37:52 -0300

    routes

--- backend/src/controllers/TournamentController.js
+++ backend/src/controllers/TournamentController.js
@@ -1,60 +1,63 @@
 const db = require("../database");
 
-
-async function getTournamentByName(req,res) {
-    const dbClient = await db.connect();
-    const tournamentName = req.body.tournamentname;
-    try{
-        const response = await db.query("SELECT * FROM tournament WHERE tournamentname = $1",[tournamentName]);
-        if(response.rowCount == 0){
-            return res.status(400).json({message:"No tournaments found"});
-        }else{
-            return res.status(200).json({data: response.rows});
-        }
-    }catch(error){
-        console.log(error);
-        return res.status(400).json({error});
-    }
-}
-
-async function getAllTournament(req,res){
-    const dbClient = await db.connect();
-    try{
-        const response = await db.query("SELECT * FROM tournament");
-        if(response.rowCount == 0){
-            return res.status(400).json({message: "No tournaments found"});
-        }else{
-            return res.status(200).json({data: response.rows});
-        }
-    }catch(error){
-        console.log(error);
-        return res.status(400).json({error});
-    }
-}
-
-async function getFormats(req,res){
+async function getTournamentByName(req, res) {
   const dbClient = await db.connect();
-  try{
-    const response = await db.query("SELECT DISTINCT format FROM tournament");
-    dbClient.release();
-    if(response.rowCount == 0){
-      return res.status(400).json({message: "No formats available"});
-    }else{
-      return res.json({data:response.rows}).status(200);
+  const tournamentName = req.body.tournamentname;
+  try {
+    const response = await db.query(
+      "SELECT * FROM tournament WHERE tournamentname = $1",
+      [tournamentName]
+    );
+    if (response.rowCount == 0) {
+      return res.status(400).json({ message: "No tournaments found" });
+    } else {
+      return res.status(200).json({ data: response.rows });
     }
-  }catch(error){
+  } catch (error) {
     console.log(error);
-    res.status(400).json({error});
+    return res.status(400).json({ error });
   }
 }
 
+async function getAllTournament(req, res) {
+  const dbClient = await db.connect();
+  try {
+    const response = await db.query("SELECT * FROM tournament");
+    if (response.rowCount == 0) {
+      return res.status(400).json({ message: "No tournaments found" });
+    } else {
+      return res.status(200).json({ data: response.rows });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error });
+  }
+}
+
+async function getFormats(req, res) {
+  const dbClient = await db.connect();
+  try {
+    const response = await db.query("SELECT DISTINCT format FROM tournament");
+    dbClient.release();
+    if (response.rowCount == 0) {
+      return res.status(400).json({ message: "No formats available" });
+    } else {
+      return res.json({ data: response.rows }).status(200);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ error });
+  }
+}
 
 async function getTournamentById(req, res) {
   const dbClient = await db.connect();
   const id = req.body.id;
 
   try {
-    const response = await db.query("SELECT * FROM tournament WHERE id = $1", [id]);
+    const response = await db.query("SELECT * FROM tournament WHERE id = $1", [
+      id,
+    ]);
     console.log(res);
     dbClient.release();
     if (response.rowCount == 0) {
@@ -68,7 +71,27 @@ async function getTournamentById(req, res) {
   }
 }
 
-async function create(req, res,) {
+async function getTournamentBySport(req, res) {
+  const dbClient = await db.connect();
+  const sport = req.body.sport;
+  try {
+    const response = await db.query(
+      "SELECT * FROM tournament WHERE sport = $1",
+      [sport]
+    );
+    dbClient.release();
+    if (response.rowCount == 0) {
+      return res.status(400).json({ message: "ERROR: Tournament not Found" });
+    } else {
+      return res.json({ message: "FOUND", data: response.rows }).status(200);
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error });
+  }
+}
+
+async function create(req, res) {
   const dbClient = await db.connect();
   const tournamentName = req.body.tournamentname;
   const joinDate = req.body.joindate;
@@ -76,35 +99,61 @@ async function create(req, res,) {
   const prize = req.body.prize;
   const format = req.body.format;
   const capacity = req.body.capacity;
+  const sport = req.body.sport;
   try {
     const response = await db.query(
-      "INSERT INTO tournament(tournamentname, joindate, tournamentdate, prize, format, capacity) VALUES ($1,$2,$3,$4,$5,$6);",
-      [tournamentName, joinDate, tournamentDate, prize, format, capacity]
+      "INSERT INTO tournament(tournamentname, joindate, tournamentdate, prize, format, capacity,sport) VALUES ($1,$2,$3,$4,$5,$6,%7);",
+      [tournamentName, joinDate, tournamentDate, prize, format, capacity, sport]
     );
-    return res.status(200).json({message:"created"});
+    return res.status(200).json({ message: "created" });
   } catch (error) {
     console.log(error);
-    return res.status(400).json({ error});
+    return res.status(400).json({ error });
   }
 }
 
-async function deleteByName(req,res) {
-    const dbClient = await db.connect();
-    const tournamentName = req.body.tournamentname;
-    try{
-        const response = await db.query(
-            "DELETE FROM tournament WHERE tournamentname = $1;"
-        ,[tournamentName]);
-        if(response.rowCount > 0){
-            return res.status(200).json({message:"removed"});
-        }else{
-            return res.status(400).json({message:"not found"})
-        }
-        
-    }catch(error){
-        console.log(error);
-        return res.status(400).json({error});
+async function deleteByName(req, res) {
+  const dbClient = await db.connect();
+  const tournamentName = req.body.tournamentname;
+  try {
+    const response = await db.query(
+      "DELETE FROM tournament WHERE tournamentname = $1;",
+      [tournamentName]
+    );
+    if (response.rowCount > 0) {
+      return res.status(200).json({ message: "removed" });
+    } else {
+      return res.status(400).json({ message: "not found" });
     }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ error });
+  }
 }
 
-module.exports = { getTournamentById ,create ,deleteByName ,getAllTournament, getTournamentByName, getFormats};
+async function getSports(req, res) {
+  const dbClient = await db.connect();
+  try {
+    const response = await db.query("SELECT * FROM sports");
+
+    if(response.rowCount > 0){
+      return res.status(200).json({message:"FOUND",data:response});
+    }else{
+      return res.status(400).json({message: "no sports found"});
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({error});
+  }
+}
+
+module.exports = {
+  getTournamentById,
+  create,
+  deleteByName,
+  getAllTournament,
+  getTournamentByName,
+  getFormats,
+  getTournamentBySport,
+  getSports
+};
